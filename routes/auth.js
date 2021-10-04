63d631a95648c8cb73b2d1dffbd800818ec132f3
dimtaiwo <dimejitaiwo@Dimejis-MacBook-Pro.local>
2021-10-04 16:33:48 +0100

    registeration route edited

--- routes/auth.js
+++ routes/auth.js
@@ -1,21 +1,21 @@
 const router = require("express").Router();
 const User = require("../models/User");
-const verify = require('../middleware/auth');
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken')
-const {loginValidation,registerValidation} = require('../validation')
+const verify = require("../middleware/auth");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const { loginValidation, registerValidation } = require("../validation");
 
 router.post("/register", async (req, res) => {
   //validate user before creating user/posting to db
   //uses registerValiation from validation.js, using Joi
   const { error } = registerValidation(req.body);
-  if (error) return res.status(400).json({"error": error.details[0].message});
+  if (error) return res.status(400).send(error.details[0].message);
 
   //check if user is already in db
   const emailExists = await User.findOne({
     email: req.body.email,
   });
-  if (emailExists) return res.status(400).json({"error": "email already in use"})
+  if (emailExists) return res.status(400).send("email already in use");
 
   //hash the password
   //using salt of 10 because I dont want the server to die
@@ -29,9 +29,19 @@ router.post("/register", async (req, res) => {
     password: hashedPassword,
   });
   try {
-    const savedUser = await user.save();
-    res.status(201).send({name : user.name, email : user.email, _id: user._id})
-    
+    await user.save();
+    //create token and give it to user
+    const token = jwt.sign({ _id: user._id }, process.env.TOKEN_SECRET, {
+      expiresIn: 3600,
+    });
+
+    res.status(201).send({
+      name: user.name,
+      email: user.email,
+      _id: user._id,
+      token: token,
+    });
+    console.log(token);
   } catch (err) {
     res.status(500).send(err);
   }
@@ -39,30 +49,30 @@ router.post("/register", async (req, res) => {
 
 //login route
 router.post("/login", async (req, res) => {
-  console.log('dealing with login')
-  console.log(req.body)
+  console.log("dealing with login");
   //validate user before creating user/posting to db
   //uses registerValiation from validation.js, using Joi
   const { error } = loginValidation(req.body);
-  if (error) return res.status(400).json({"error": error.details[0].message});
+  if (error) return res.status(400).send(error.details[0].message);
   //check if user (email) is already in db
   const user = await User.findOne({
     email: req.body.email,
   });
-  if (!user) return res.status(400).json({"error": "email not found"})
+  if (!user) return res.status(400).send("email not found");
   //check if password is correct
   const validPass = await bcrypt.compare(req.body.password, user.password);
-  if (!validPass) return res.status(400).json({"error": "incorrect password"})
-  
+  if (!validPass) return res.status(400).send("password wrong");
+
   //create token and give it to user
-  const token = jwt.sign({_id: user._id}, process.env.TOKEN_SECRET, {expiresIn: 3600000000});
+  const token = jwt.sign({ _id: user._id }, process.env.TOKEN_SECRET, {
+    expiresIn: 3600,
+  });
   // console.log('about to make cookie')
   // res.cookie('token',token, {httpOnly:true, domain:"http://127.0.0.1:5500", sameSite: "none", secure: true})
-  res.header('auth-token', token)
-  res.status(200).json({token})
+  res.header("auth-token", token);
+  res.status(200).send("successfully logged in");
 
-
-//   res.send("logged in");
+  //   res.send("logged in");
 });
 
 module.exports = router;
