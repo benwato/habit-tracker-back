ef714c05f49f6c03aad668cc4dc6971a27cc7fdc
brodie-m <brodiedmcguire@gmail.com>
2021-10-04 16:40:51 +0100

    changed error messages

--- routes/auth.js
+++ routes/auth.js
@@ -9,13 +9,13 @@ router.post("/register", async (req, res) => {
   //validate user before creating user/posting to db
   //uses registerValiation from validation.js, using Joi
   const { error } = registerValidation(req.body);
-  if (error) return res.status(400).send(error.details[0].message);
+  if (error) return res.status(400).json({"error": error.details[0].message});
 
   //check if user is already in db
   const emailExists = await User.findOne({
     email: req.body.email,
   });
-  if (emailExists) return res.status(400).send("email already in use");
+  if (emailExists) return res.status(400).json({"error": "email already in use"})
 
   //hash the password
   //using salt of 10 because I dont want the server to die
@@ -53,15 +53,15 @@ router.post("/login", async (req, res) => {
   //validate user before creating user/posting to db
   //uses registerValiation from validation.js, using Joi
   const { error } = loginValidation(req.body);
-  if (error) return res.status(400).send(error.details[0].message);
+  if (error) return res.status(400).json({"error": error.details[0].message});
   //check if user (email) is already in db
   const user = await User.findOne({
     email: req.body.email,
   });
-  if (!user) return res.status(400).send("email not found");
+  if (!user) return res.status(400).json({"error": "email not found"})
   //check if password is correct
   const validPass = await bcrypt.compare(req.body.password, user.password);
-  if (!validPass) return res.status(400).send("password wrong");
+  if (!validPass) return res.status(400).json({"error": "incorrect password"})
 
   //create token and give it to user
   const token = jwt.sign({ _id: user._id }, process.env.TOKEN_SECRET, {
