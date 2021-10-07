3df80e9b11fe6a59f958894fe71c517ccb30a919
brodie-m <brodiedmcguire@gmail.com>
2021-10-07 08:08:26 +0100

    added port to index

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose')
 //.env config
 dotenv.config();
 
-app.listen(3000, () => console.log('server up and running'))
+app.listen(process.env.PORT, () => console.log('server up and running'))
 
 //connect to db
 mongoose.connect(process.env.DB_CONNECT, {
